55bc6ffd018857ffa170989c881761a315cef4c0
ningliu <ningliu@sf-express.com>
2019-12-07 17:32:35 +0800

    创建打卡

--- src/pages/edit/edit.js
+++ src/pages/edit/edit.js
@@ -1,1 +1,11 @@
-Page({});
+Page({
+  chooseImg() {
+    wx.chooseImage({
+      count: 1,
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        console.log(res.tempFilePaths);
+      },
+    });
+  },
+});
