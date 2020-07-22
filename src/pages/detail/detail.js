9216d8340f7f7e10668b1be984d1c674ddb55550
liuning <liuning@liuning.local>
2020-07-22 14:48:56 +0800

    update

--- src/pages/detail/detail.js
+++ src/pages/detail/detail.js
@@ -1,14 +1,25 @@
 import request from '@/utils/request';
+import { format } from '@/utils/date';
 
 Page({
   data: {
     isDone: false,
     iPhone: false,
     detail: {},
+    taskId: null,
   },
   async onLoad({ task_id }) {
     const data = await request('/api/xrm/task/detail', { params: { id: task_id } });
-    this.setData({ detail: data });
+    // 判断今日是否已打卡
+    if (data && data.last_submit_date) {
+      const timestamp = Number(new Date()).toString().slice(0, 10);
+      if (Number(format('YYYYMMDD', timestamp)) === data.last_submit_date) {
+        this.setData({ isDone: true });
+      }
+    }
+
+    this.setData({ detail: data, taskId: task_id });
+
     wx.getSystemInfo({
       success: (res) => {
         this.setData({ iPhone: res.model.indexOf('iPhone') > -1 });
@@ -24,7 +35,14 @@ Page({
   goEdit() {
     wx.navigateTo({ url: '/pages/edit/edit?edit=1' });
   },
-  touchDaka() {
-    this.setData({ isDone: true });
+  async touchDaka(e) {
+    const { id } = e.currentTarget.dataset;
+    const { taskId } = this.data;
+
+    const result = await request('/api/xrm/task/submit', { type: 'POST', params: { id } });
+    if (result) {
+      const data = await request('/api/xrm/task/detail', { params: { id: taskId } });
+      this.setData({ isDone: true, detail: data });
+    }
   },
 });
